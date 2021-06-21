57210197391d6c450ef1a964a3ae391b9efcdb8f
ljung <ljungmanjimmy@hotmail.com>
2021-06-21 20:15:14 +0200

    - added loqo_experiment.ipynb code

--- utils/utils.py
+++ utils/utils.py
@@ -22,15 +22,19 @@
 
     * scramble_texts - Takes a list of texts and scrambles the words in each text.
 
+    * average_report - Takes a list of dict-like sklearn classification reports and averages them.
+
+    * format_report - Takes a dict-like classification report in the format given by average_report() and formats
+    it into a readable printable string.
 """
 
-from collections import OrderedDict
 import pandas as pd
 import re
 from sklearn.feature_extraction.text import CountVectorizer
 from statistics import mean, median
 import random
 from random import shuffle
+
 
 def process_texts(texts=None, punctuations=True, numbers=True, single_character=True):
     """ Takes a list of texts and returns the preprocessed versions.
@@ -315,3 +319,71 @@ def scramble_texts(texts=None, random_state=None):
         new_sentence = scramble_text(sentence, random_state)
         scrambled_data.append(new_sentence)
     return scrambled_data
+
+
+def average_report(classification_reports):
+    """Takes a list of dict-like sklearn classification reports
+    (See: https://scikit-learn.org/stable/modules/generated/sklearn.metrics.classification_report.html), and returns
+    the average of all metrics between the reports.
+
+    :param classification_reports: list of dict-like sklearn classification reports.
+    :return: a dict of averaged metrics between the reports.
+    """
+    keys = classification_reports[0].keys()
+    class_metrics = classification_reports[0][list(keys)[0]].keys()
+
+    # create template for new report
+    averaged_report = {}
+    for key in keys:
+        if key == 'accuracy':
+            averaged_report[key] = []
+            continue
+        else:
+            averaged_report[key] = {}
+        for metric in class_metrics:
+            averaged_report.get(key)[metric] = []
+
+    # add values to template
+    for classification_report in classification_reports:
+        for key in keys:
+
+            if key == 'accuracy':
+                averaged_report.get(key).append(classification_report[key])
+                continue
+
+            metrics_map = classification_report[key]
+
+            for metric in class_metrics:
+                score = metrics_map[metric]
+                averaged_report.get(key).get(metric).append(score)
+
+    # average values
+    for label, metrics_map in averaged_report.items():
+        if label == 'accuracy':
+            averaged_report[label] = mean(metrics_map)
+            continue
+
+        for metric, scores in metrics_map.items():
+            metrics_map[metric] = mean(scores)
+
+    return averaged_report
+
+
+def format_report(report):
+    """Takes a dict-like classification report in the format given by average_report() and formats it into
+    a readable printable string.
+
+    :param report: dict-like classification report in the format given by average_report().
+    :return: readable print string.
+    """
+    formated_report = ''
+    for label, metrics in report.items():
+        formated_report += '{}-> '.format(label)
+        if type(metrics) == dict:
+            for metric, score in metrics.items():
+                formated_report += '{}: {:.2f}, '.format(metric, score)
+        else:
+            formated_report += '{:.2f}'.format(metrics)
+        formated_report += '\n'
+
+    return formated_report
